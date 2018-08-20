fa7e08d23716c1d3331ab707191536d4758fb0f5
designMoreWeb <divystolia@gmail.com>
2018-08-20 11:52:35 -0400

    file setup with time and random

--- snakeGame.py
+++ snakeGame.py
@@ -2,3 +2,5 @@
 
 import pygame #for helping in creating the game
 import sys #for 3d modeling and using system modules
+import random #random number for the food apearing randomly 
+import time #useful for exit functions
