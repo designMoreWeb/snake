3acb91101f21453d35b46dd3c6b18cfe2782c0b8
designMoreWeb <divystolia@gmail.com>
2018-08-20 12:09:08 -0400

    play surface created

--- snakeGame.py
+++ snakeGame.py
@@ -7,3 +7,15 @@
 # Time: useful for exit functions
 
 import pygame, sys, random, time
+
+check_errors = pygame.init() #initializing pygame
+if check_errors[1] > 0:
+    print("(!) Had {0} initialziing errors,exiting...".format(check_errors[1]))
+    sys.exit(-1)
+else:
+    print("(+) Game is initialized!")
+
+#Creating the game surface
+
+playSurface = pygame.display.set_mode((800,400))
+
