e4102c37b80b1792e5bae2c82cebea38c34381a3
designMoreWeb <divystolia@gmail.com>
2018-08-20 17:33:10 -0400

    added direction constraints

--- snakeGame.py
+++ snakeGame.py
@@ -1,75 +1,89 @@
 # Snake Game in python 3.7 using pygame
 
-#Imports
-# Pygame: for helping in creating the game 
-# SYS: for 3d modeling and using system modules 
-# Random: random number for the food apearing randomly  
+# Imports
+# Pygame: for helping in creating the game
+# SYS: for 3d modeling and using system modules
+# Random: random number for the food apearing randomly
 # Time: useful for exit functions
 
-import pygame, sys, random, time
+import pygame
+import sys
+import random
+import time
 
-check_errors = pygame.init() #initializing pygame
+check_errors = pygame.init()  # initializing pygame
 if check_errors[1] > 0:
     print("(!) Had {0} initialziing errors,exiting...".format(check_errors[1]))
     sys.exit(-1)
 else:
     print("(+) Game is initialized!")
 
-#Creating the game surface
+# Creating the game surface
 
-playSurface = pygame.display.set_mode((800,400))
+playSurface = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Snake Game')
 
-# Colors for the game 
-red = pygame.Color(255, 0, 0) #gameover
-green = pygame.Color(0, 255, 0) #snake
-black = pygame.Color(0, 0, 0) #score
-white = pygame.Color(255, 255, 255) #background
-brown = pygame.Color(165, 42, 42) #food
+# Colors for the game
+red = pygame.Color(255, 0, 0)  # gameover
+green = pygame.Color(0, 255, 0)  # snake
+black = pygame.Color(0, 0, 0)  # score
+white = pygame.Color(255, 255, 255)  # background
+brown = pygame.Color(165, 42, 42)  # food
 
 # Control the frames
 frameController = pygame.time.Clock()
 
 # variables
 snakePos = [100, 50]
-snakeBody = [[100,50], [90,50], [80,50]]
+snakeBody = [[100, 50], [90, 50], [80, 50]]
 
-foodPos = [random.randrange(1,80)*10, random.randrange(1,40)*10]
+foodPos = [random.randrange(1, 80)*10, random.randrange(1, 40)*10]
 foodSpawn = True
 
 direction = 'RIGHT'
 changeTo = direction
 
-#creating function for gameisOver
+# creating function for gameisOver
+
+
 def gameOver():
     myFont = pygame.font.SysFont('arial', 50)
     gameIsOverSurf = myFont.render('Game Over!', True, red)
     gameIsOverRect = gameIsOverSurf.get_rect()
-    gameIsOverRect.midtop = (400,25)
-    playSurface.blit(gameIsOverSurf,gameIsOverRect)
+    gameIsOverRect.midtop = (400, 25)
+    playSurface.blit(gameIsOverSurf, gameIsOverRect)
     pygame.display.flip()
     time.sleep(10)
-    pygame.quit() #snake game exit
-    sys.exit() #console exit
+    pygame.quit()  # snake game exit
+    sys.exit()  # console exit
 
 # Events
 
+
 # the main logic for the game
-while 1:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT or event.key == ord('d'):
-                changeTo =="RIGHT"
+                changeTo == "RIGHT"
             if event.key == pygame.K_LEFT or event.key == ord('a'):
-                changeTo =="LEFT"
+                changeTo == "LEFT"
             if event.key == pygame.K_UP or event.key == ord('w'):
-                changeTo =="UP"
+                changeTo == "UP"
             if event.key == pygame.K_DOWN or event.key == ord('s'):
-                changeTo =="DOWN"
+                changeTo == "DOWN"
             if event.key == pygame.K_ESCAPE:
                 pygame.event.post(pygame.event.Event((QUIT)))
 
-
+# Validate for directions 
+if changeTo == 'RIGHT' and not direction == 'LEFT':
+    direction = 'RIGHT'
+if changeTo == 'LEFT' and not direction == 'RIGHT':
+    direction = 'LEFT'
+if changeTo == 'UP' and not direction == 'DOWN':
+    direction = 'UP'
+if changeTo == 'DOWN' and not direction == 'UP':
+    direction = 'DOWN'
