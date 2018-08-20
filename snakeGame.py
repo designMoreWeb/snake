f1f31f6a74a42b26cec362dd97205689abab276b
designMoreWeb <divystolia@gmail.com>
2018-08-20 18:07:18 -0400

    add food location

--- snakeGame.py
+++ snakeGame.py
@@ -6,10 +6,7 @@
 # Random: random number for the food apearing randomly
 # Time: useful for exit functions
 
-import pygame
-import sys
-import random
-import time
+import pygame, sys, random, time
 
 check_errors = pygame.init()  # initializing pygame
 if check_errors[1] > 0:
@@ -54,10 +51,10 @@ def gameOver():
     playSurface.blit(gameIsOverSurf, gameIsOverRect)
     pygame.display.flip()
 
-
     time.sleep(10)
     pygame.quit()  # snake game exit
     sys.exit()  # console exit
+
 
 # the main logic for the game
 while True:
@@ -77,7 +74,7 @@ while True:
             if event.key == pygame.K_ESCAPE:
                 pygame.event.post(pygame.event.Event((QUIT)))
 
-# Validate for directions 
+# Validate for directions
 if changeTo == 'RIGHT' and not direction == 'LEFT':
     direction = 'RIGHT'
 if changeTo == 'LEFT' and not direction == 'RIGHT':
@@ -97,8 +94,8 @@ if direction == 'DOWN':
     snakePos[1] += 10
 
 # SNAKE body Movement
-snakeBody.insert(0,list(snakePos))
-if snakePos[0] == foodPos[0] and snakePos[1] == foodPos [1]:
+snakeBody.insert(0, list(snakePos))
+if snakePos[0] == foodPos[0] and snakePos[1] == foodPos[1]:
     foodSpawn = False
 else:
     snakeBody.pop()
@@ -112,5 +109,7 @@ playSurface.fill(white)
 for pos in snakeBody:
     pygame.draw.rect(playSurface, green, pygame.RECT(pos[0], pos[1], 10, 10))
 
+pygame.draw.rect(playSurface, brown, pygame.RECT(foodPos[0], foodPos[1], 10, 10))
+
 pygame.display.flip()
-frameController.tick(25)
+frameController.tick(10)
