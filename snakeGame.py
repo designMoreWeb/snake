f164863f550fe996f110954c301c28faf398e468
designMoreWeb <divystolia@gmail.com>
2018-08-20 11:57:34 -0400

    comments

--- snakeGame.py
+++ snakeGame.py
@@ -1,6 +1,9 @@
 # Snake Game in python 3.7 using pygame
 
-import pygame #for helping in creating the game
-import sys #for 3d modeling and using system modules
-import random #random number for the food apearing randomly 
-import time #useful for exit functions
+#Imports
+# Pygame: for helping in creating the game 
+# SYS: for 3d modeling and using system modules 
+# Random: random number for the food apearing randomly  
+# Time: useful for exit functions
+
+import pygame, sys, random, time
