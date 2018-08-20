10763f397183353485ab1cd30b0cdf278e42b9e7
designMoreWeb <divystolia@gmail.com>
2018-08-20 12:22:24 -0400

    create a frame timer to controller the movement of the snake, and created varables for snake position and snake body position

--- snakeGame.py
+++ snakeGame.py
@@ -26,3 +26,11 @@ green = pygame.Color(0, 255, 0) #snake
 black = pygame.Color(0, 0, 0) #score
 white = pygame.Color(255, 255, 255) #background
 brown = pygame.Color(165, 42, 42) #food
+
+# Control the frames
+frameController = pygame.time.Clock()
+
+# variables
+snakePos = [100, 50]
+snakeBody = [[100,50][90,50][80,50]]
+
